e509a228b86ac9533ddf76279c08b8f484a58abb
Kawsar Ahmed <76034051+sami019325@users.noreply.github.com>
2025-11-21 07:36:59 +0600

    Delete routes directory

--- routes/models/hash.js
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcrypt";
-
-const password = "adminpassword";
-const saltRounds = 10;
-
-bcrypt.hash(password, saltRounds).then(hash => {
-    console.log("Hashed password:", hash);
-});
